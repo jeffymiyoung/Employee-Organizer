6fe3aa5224f305bdeb0ae45a106b0eac65b044eb
Jeffrey Young <jeffreymiyoung@aol.com>
2022-05-30 16:10:05 -0400

    refactored viewer equations

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const validate = require('validator');
 const figlet = require('figlet');
 const chalk = require('chalk');
 
+// Linking Equations
+const { allEmployees, allRoles, allDepartments, EmployeeDepartments, departmentBudgets} = require('./observe/viewall.js');
+
 // DB Connection and opening title
 connection.connect((error) => {
     if(error) throw error;
@@ -95,100 +98,32 @@ const promptUser = () => {
 //  -- SQL Functions --
 // View All Employees
 const viewEmployees = () => {
-    const sql = `SELECT employees.id, employees.first_name, employees.last_name, roles.title, department.department_name AS department, roles.salary
-                FROM employees, roles, department
-                WHERE department.id = roles.department_id
-                AND roles.id = employees.role_id
-                ORDER BY employees.id ASC`;
-
-    connection.query(sql, (error, res) => {
-        if (error) throw new Error;
-
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        console.log(``);
-        console.log(chalk.magentaBright.bold(figlet.textSync(`Current  Employees:`)));
-        console.log(``);
-        console.table(res);
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        promptUser();
-    });
+    allEmployees();
+    setTimeout(promptUser, 300);
 };
 
 // View All Roles
 const viewRoles = () => {
-    const sql = `SELECT roles.id, roles.title, department.department_name AS department, roles.salary
-                    FROM roles
-                    INNER JOIN department ON roles.department_id = department.id`;
-
-    connection.query(sql, (error, res) => {
-        if (error) throw new Error;
-
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        console.log(``);
-        console.log(chalk.magentaBright.bold(figlet.textSync(`Current  Employee  Roles:`)));
-        console.log(``);
-        console.table(res);
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        promptUser();
-    });
+    allRoles();
+    setTimeout(promptUser, 300);
 };
 
 // View All Departments
 const viewDepartments = () => {
-    const sql = `SELECT department.id AS id, department.department_name AS department
-                    FROM department`;
-
-    connection.query(sql, (error, res) => {
-        if (error) throw new Error;
-
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        console.log(``);
-        console.log(chalk.magentaBright.bold(figlet.textSync(`Current  Departments:`)));
-        console.log(``);
-        console.table(res);
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        promptUser();
-    });
+    allDepartments();
+    setTimeout(promptUser, 300);
 };
 
 // View All Employees by Department
 const viewEmployeesByDepartment = () => {
-    const sql = `SELECT employees.first_name, employees.last_name, department.department_name AS department
-                    FROM employees
-                    LEFT JOIN roles ON employees.role_id = roles.id
-                    LEFT JOIN department ON roles.department_id = department.id`;
-
-    connection.query(sql, (error, res) => {
-        if (error) throw new Error;
-
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        console.log(``);
-        console.log(chalk.magentaBright.bold(figlet.textSync(`Employees  by  Department:`)));
-        console.log(``);
-        console.table(res);
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        promptUser();
-    });
+    EmployeeDepartments();
+    setTimeout(promptUser, 300);
 };
 
 // View Department Budgets
 const viewDepartmentBudget = () => {
-    const sql = `SELECT department_id AS id, department.department_name AS department, 
-                    SUM(salary) AS budget
-                    FROM roles
-                    INNER JOIN department ON roles.department_id = department.id GROUP BY roles.department_id`;
-
-    connection.query(sql, (error, res) => {
-        if (error) throw new Error;
-
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        console.log(``);
-        console.log(chalk.magentaBright.bold(figlet.textSync(`Department  Budgets:`)));
-        console.log(``);
-        console.table(res);
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        promptUser();
-    });
+    departmentBudgets();
+    setTimeout(promptUser, 300);
 };
 
 // Update Employee Role
