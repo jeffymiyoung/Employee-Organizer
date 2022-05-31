08b15a66131996cd162ec818dd0b0347fb6f5897
Jeffrey Young <jeffreymiyoung@aol.com>
2022-05-30 22:36:06 -0400

    Finalized deletions

--- delete/deletes.js
+++ delete/deletes.js
@@ -48,36 +48,80 @@ const minusEmployee = () => {
 
 // Remove Role
 const minusRole = () => {
-    const sql = ``;
-
+    const sql = `SELECT roles.id, roles.title FROM roles`;
     connection.query(sql, (error, res) => {
         if (error) throw new Error;
+        let rolesArray = [];
+        res.forEach((roles) => {rolesArray.push(roles.title)});
 
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        console.log(``);
-        console.log(chalk.magentaBright.bold(figlet.textSync(``)));
-        console.log(``);
-        console.table(res);
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        promptUser();
+        inquirer.prompt([
+            {
+                name: 'selectRole',
+                type: 'list',
+                message: 'Which role would you like to remove?',
+                choices: rolesArray
+            }
+        ])
+        .then((answer) => {
+            let roleId;
+            res.forEach((roles) => {
+                if (answer.selectRole === roles.title) {
+                    roleId = roles.id;
+                }
+            });
+            
+            const sql = `DELETE FROM roles
+                            WHERE roles.id = ?`;
+            connection.query(sql, [roleId], (error, res) => {
+                if (error) throw new Error;
+
+                console.log(chalk.red.bold(`====================================================================================`));
+                console.log(``);
+                console.log(chalk.redBright.bold(figlet.textSync(`Role Successfully Removed!`)));
+                console.log(``);
+                console.log(chalk.red.bold(`====================================================================================`));
+            });
+        });
     });
 };
 
 // Remove Department
 const minusDepartment = () => {
-    const sql = ``;
-
+    const sql = `SELECT department.id, department.department_name FROM department`;
     connection.query(sql, (error, res) => {
         if (error) throw new Error;
+        let departmentsArray = [];
+        res.forEach((department) => {departmentsArray.push(department.department_name)});
 
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        console.log(``);
-        console.log(chalk.magentaBright.bold(figlet.textSync(``)));
-        console.log(``);
-        console.table(res);
-        console.log(chalk.cyan.bold(`====================================================================================`));
-        promptUser();
-    });
+        inquirer.prompt([
+            {
+                name: 'selectDepartment',
+                type: 'list',
+                message: 'Which Department would you like to remove?',
+                choices: departmentsArray
+            },
+        ])
+        .then((answer) => {
+            let departmentId;
+            res.forEach((department) => {
+                if (answer.selectDepartment === department.department_name) {
+                    departmentId = department.id;
+                }
+            });
+            const sql = `DELETE FROM department
+                            WHERE department.id = ?`;
+            connection.query(sql, [departmentId], (error, res) => {
+                if (error) throw new Error;
+
+                console.log(chalk.red.bold(`====================================================================================`));
+                console.log(``);
+                console.log(chalk.redBright.bold(figlet.textSync(`Role Successfully Removed!`)));
+                console.log(``);
+                console.log(chalk.red.bold(`====================================================================================`));
+            })
+        })
+    })
+
 };
 
 // Export for External
