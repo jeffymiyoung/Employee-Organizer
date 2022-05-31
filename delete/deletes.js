7d30253ccf6148a20ef57a161a20cb4b0ed8d794
Jeffrey Young <jeffreymiyoung@aol.com>
2022-05-30 22:22:24 -0400

    deleteEmployee addition

--- delete/deletes.js
+++ delete/deletes.js
@@ -8,18 +8,41 @@ const chalk = require('chalk');
 
 // Remove Employee
 const minusEmployee = () => {
-    const sql = ``;
+    const sql = `SELECT employees.id, employees.first_name, employees.last_name FROM employees`;
 
     connection.query(sql, (error, res) => {
         if (error) throw new Error;
+        let employeesArray = [];
+        res.forEach((employees) => {employeesArray.push(`${employees.first_name} ${employees.last_name}`)});
 
-        console.log(chalk.red.bold(`====================================================================================`));
-        console.log(``);
-        console.log(chalk.redBright.bold(figlet.textSync(``)));
-        console.log(``);
-        console.table(res);
-        console.log(chalk.red.bold(`====================================================================================`));
-        promptUser();
+        inquirer.prompt([
+            {
+                name: 'selectEmployee',
+                type: 'list',
+                message: 'Which Employee would you like to remove?',
+                choices: employeesArray
+            },
+        ])
+        .then((answer) => {
+            let employeeId;
+
+            res.forEach((employees) => {
+                if (answer.selectEmployee === `${employees.first_name} ${employees.last_name}`) {
+                    employeeId = employees.id;
+                }
+            });
+            const sql = `DELETE FROM employees
+                            WHERE employees.id =?`;
+            connection.query(sql, [employeeId], (error, res) => {
+                if(error) throw new Error;
+
+                console.log(chalk.red.bold(`====================================================================================`));
+                console.log(``);
+                console.log(chalk.redBright.bold(figlet.textSync(`Employee Successfully Removed!`)));
+                console.log(``);
+                console.log(chalk.red.bold(`====================================================================================`));
+            });
+        });
     });
 };
 
